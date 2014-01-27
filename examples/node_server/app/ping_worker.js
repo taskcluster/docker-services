051d57e1a093b3c8f04fe7732a0c99c1fd3325c3
James Lal <james@lightsofapollo.com>
2014-01-26 19:02:13 -0800

    Actually add examples needed to run the tests

---
+++ examples/node_server/app/ping_worker.js
@@ -0,0 +1,30 @@
+var URL = require('url');
+var net = require('net');
+var assert = require('assert');
+
+assert(process.env.WORKER_PORT, 'has worker');
+var parsed = URL.parse(process.env.WORKER_PORT);
+
+process.stdout.write('stdout');
+process.stderr.write('stderr');
+
+function connectToWorker() {
+  console.log('connecting....', process.env);
+  var client = net.createConnection(
+    parsed.port,
+    parsed.hostname,
+    function connected(socket) {
+      console.log('connected');
+      client.end();
+      console.log('--- REQUEUE ---');
+      setTimeout(connectToWorker, 2000);
+    }
+  );
+
+  client.setTimeout(1000, function() {
+    console.log('failed to connect to socket');
+    process.exit(1);
+  });
+}
+
+connectToWorker();
